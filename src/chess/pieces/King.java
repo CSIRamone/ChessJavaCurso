5073793b202aeccdb8b112ff3e2cc3651d5d721c
CSIRamone <cidinei_inacio@hotmail.com>
2020-01-03 21:03:48 -0300

    Possible moves of a piece

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -14,6 +14,12 @@ public class King extends ChessPiece{
 	public String toString() {
 		return "K";
 	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] matrix = new boolean[getBoard().getRows()][getBoard().getColumns()]; 
+		return matrix;
+	}
 	
 
 }
