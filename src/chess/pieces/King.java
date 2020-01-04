2ebc084bc30240c2f9f2776e05924a3347c955a6
CSIRamone <cidinei_inacio@hotmail.com>
2020-01-04 17:53:33 -0300

    Implementing possible moves of King

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -15,9 +16,62 @@ public class King extends ChessPiece{
 		return "K";
 	}
 
+	private boolean canMove(Position position) {
+		ChessPiece auxPiece = (ChessPiece)getBoard().piece(position);
+		return auxPiece == null || auxPiece.getColor() != getColor();
+	}
+	
 	@Override
 	public boolean[][] possibleMoves() {
-		boolean[][] matrix = new boolean[getBoard().getRows()][getBoard().getColumns()]; 
+		boolean[][] matrix = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		Position auxPosition = new Position(0,0);
+		
+		// acima
+		auxPosition.setValues(position.getRow() - 1, position.getColumn());
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			matrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+		// abaixo
+		auxPosition.setValues(position.getRow() + 1, position.getColumn());
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			matrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+		// esquerda 
+		auxPosition.setValues(position.getRow(), position.getColumn() - 1);
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			matrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+		// direita
+		auxPosition.setValues(position.getRow(), position.getColumn() + 1);
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			matrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+		// noroeste
+		auxPosition.setValues(position.getRow() - 1, position.getColumn() - 1);
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			matrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+		// nordeste
+		auxPosition.setValues(position.getRow() - 1, position.getColumn() + 1);
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			matrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+		// sudoeste
+		auxPosition.setValues(position.getRow() + 1, position.getColumn() - 1);
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			matrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+		// sudeste 
+		auxPosition.setValues(position.getRow() + 1, position.getColumn() + 1);
+		if (getBoard().positionExists(auxPosition) && canMove(auxPosition)) {
+			matrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		}
+		
+		
+		
+		
+		
+		
 		return matrix;
 	}
 	
